58fcaa35949e77b872bf94d350894df0f3fd7bb0
ch90331 <62222044+ch90331@users.noreply.github.com>
2020-06-13 11:26:20 +0100

    Created and styled the component "Footer".

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p className="github">
+        <a href="https://github.com/ch90331/Ya-Ju--Weather-App" target="_blank">
+          Open-source code
+        </a>
+        on GitHub by Ya-Ju Yu
+      </p>
+    </div>
+  );
+}
