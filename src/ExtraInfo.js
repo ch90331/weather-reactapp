9c33c68cc83284c64dd34646766b9dcc36f2242c
ch90331 <62222044+ch90331@users.noreply.github.com>
2020-06-23 20:17:23 +0100

    Added the real-time data on ExtraInfo.js

--- src/ExtraInfo.js
+++ src/ExtraInfo.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from "axios";
 
-export default function ExtraInfo() {
+export default function ExtraInfo(prop) {
+  const [weatherData, setWeatherData]=useState({show:false});
+  function weatherResponse(response){
+    console.log(response.data)
+    setWeatherData({
+      show: true,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  if (weatherData.show){
   return (
     <div className="ExtraInfo">
-      💧 Humidity: 77%
-      <div> 🌬 Wind: 7.2 km/h</div>
+      💧 Humidity: {weatherData.humidity}%
+      <div> 🌬 Wind: {weatherData.wind} km/h</div>
     </div>
   );
+  }else{
+    const apiKey="2705c3833e0eb8cc3d104831dddd5c14";
+    let apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=${prop.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(weatherResponse);
+
+    return"Loading....";
+  }
 }
