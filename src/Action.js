a0b8dba2a295e5f27b77c6b8376f2d6117121160
ch90331 <62222044+ch90331@users.noreply.github.com>
2020-07-14 06:13:22 +0100

    Added new functions to the component Action.js (testing phase)

--- src/Action.js
+++ src/Action.js
@@ -1,13 +1,43 @@
-import React from "react";
-import Mango_Shaved_Ice from "./Mango_Shaved_Ice.png";
+import React,{useState} from "react";
+import Mango_Shaved_Ice from "./Img/Mango_Shaved_Ice.png";
 
-export default function Action (){
+import "./Action.css";
 
+
+export default function Action (props){
+  const [comment, setComment]=useState(null);
+  const [img,setImg]=useState("Mango_Shaved_Ice");
+  const [object, setObject]=useState(null);
+  
+  function changeToHot (){
+    setComment("Time to indulge in a delicious golden treat 😋:");
+    setImg("Mango_Shaved_Ice");
+    setObject("Mango Shaved Ice");
+  }
+
+  function changeToFreezinglyCold(){
+    setComment("Feeling freezingly cold 🥶 ? Taiwanese rice cooker is your best tool to prepare a nostalgic soup 🥣");
+    setImg("Rice_cooker");
+    setObject(null);
+  }
+
+  if (props.temperature>30){
+    changeToHot();
     return(
-    <div className= "Action">
-      <div> Time to indulge in a delicious golden treat: </div>
-      <img src={Mango_Shaved_Ice} alt="hot" width="50%" />
-      <div> Mango Shaved Ice </div>
-    </div>
-    )
+      <div className= "Action">
+       <div> {comment} </div>
+       <img src={require(`./Img/${img}.png`)} alt="hot" width="35%" />
+       <div> {object} </div>
+      </div>
+    );
+  }else {
+    changeToFreezinglyCold();
+    return(
+      <div className= "Action">
+       <div> {comment} </div>
+       <img src={require(`./Img/${img}.png`)} alt="hot" width="35%" />
+       <div> {object} </div>
+      </div>
+    );
+  }
 }
