17b8c363bde75a618adf87c3662422cf76971944
ch90331 <62222044+ch90331@users.noreply.github.com>
2020-08-15 16:51:01 +0100

    Added className in FormattedTime.js

--- src/FormattedTime.js
+++ src/FormattedTime.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function FormattedTime (props){
   
  return (
- <div>
+ <div className="FormattedTime">
  <span> <FontAwesomeIcon icon="calendar-alt" size="lg"/> {props.day}</span>
  <span> <FontAwesomeIcon icon="clock" size="lg" /> {props.realHour}:{props.min} </span>
  </div>
