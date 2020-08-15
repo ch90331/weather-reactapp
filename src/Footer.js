a9298f1bd5c00ae44c01f25f9609e122ef187dbc
ch90331 <62222044+ch90331@users.noreply.github.com>
2020-08-15 19:43:54 +0100

    Corrected the warning- Added rel="noopener noreferrer" to <a>

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="Footer">
       <p className="github">
-        <a href="https://github.com/ch90331/weather-reactapp" target="_blank">
+        <a rel="noopener noreferrer" href="https://github.com/ch90331/weather-reactapp" target="_blank">
           Open-source code
         </a>
         {" "} on GitHub by Ya-Ju Yu
